926d9bdb3f05f79d274df39deee8ef7c0fbfaf7d
andrew <example@gmail.com>
2020-09-24 16:20:29 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,28 @@
+ "use strict";
+
+//const result = confirm("Are you here?");
+
+//console.log(result);
+
+//const answer = +prompt("Вам есть 18?", "18");
+
+//console.log(answer + 5);
+
+const answers=[];
+
+answers[0] = prompt('What is you name?', '');
+answers[1] = prompt('Wat is you surename?', '');
+answers[2] = prompt('How old are you?', '');
+
+document.write(answers);
+
+ 
+
+
+
+
+
+
+
+
+
